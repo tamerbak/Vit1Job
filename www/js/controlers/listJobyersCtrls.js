bf23a71e5121a49c47fb0f5b37944ddfeaf7b44f
kbourzayq <khalid.bourzayq@gmail.com>
2015-11-21 23:57:13 +0000

    www/js/controlers/listJobyersCtrls.js www/templates/listJobyers.html

--- www/js/controlers/listJobyersCtrls.js
+++ www/js/controlers/listJobyersCtrls.js
@@ -3,8 +3,36 @@
  */
 'use strict';
 starter
-  .controller('listCtrl', function ($scope, $rootScope) {
+  .controller('listCtrl', function ($scope,$ionicActionSheet, $rootScope) {
     $scope.jobyersForMe = $rootScope.jobyersForMe;
 
+    $scope.showMenuForContract = function(jobber){
+      var hideSheet = $ionicActionSheet.show({
+        buttons: [
+          { text: '<i class="ion-ios-paper-outline"> Créer un contrat</i>' }
+        ],
+        titleText: 'Contractualisation',
+        cancelText: 'Annuler',
+        buttonClicked: function(index) {
+          //branchement de la page de contrat ou infos clients
+          if(index==0){
+            /*
+              recuperation des données de l'emplyeur et calcule dans une variable boolean
+              si toutes les informations sont présentes
+            */
+            console.log(jobber);
+            var dataInformed = false; // TODO
+            //test si tous les infomations de l'employeur sont renseignées
+            if(dataInformed){
+              //show contract page //TODO
+            }
+            else{
+              //show employer infos page //TODO
+            }
+          }
+          return true;
+        }
+      });
+    }
   })
 ;

56d7d625f5c22464d5da482530cf327fba3cdc2a
SoftBrain002 <m.aitbenadi@softbrain-it.com>
2016-01-11 19:20:55 +0000

    fix popup geolocation

--- www/js/services/geoService.js
+++ www/js/services/geoService.js
@@ -50,7 +50,7 @@ services
              deferred.reject(error);
            };
             console.log("befooore geolocation");
-           navigator.geolocation.getCurrentPosition(onSuccess, onError,{timeout: 5000});
+           navigator.geolocation.getCurrentPosition(onSuccess, onError,{timeout: 1000});
 
           return deferred.promise;
         },
